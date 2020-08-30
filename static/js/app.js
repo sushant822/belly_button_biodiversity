ed0696c4e8b9e78936694a3b87bb8ba43e188d0d
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-08-30 17:56:47 -0600

    Modified app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,21 @@
 var url = "samples.json";
 
-d3.json(url).then(data => console.log(data));
+function unpack(rows, index) {
+    return rows.map(row => row[index]);
+  }
+
+d3.json(url).then(function(data) {
+    var sampleValues = data.samples[0].sample_values.slice(0, 10);
+    var otuIDS = data.samples[0].otu_ids;
+    var otuLables = data.samples[0].otu_labels;
+    //console.log(otuLables);
+    var data = [{
+        type: 'bar',
+        x: sampleValues,
+        y: otuLables,
+        orientation: 'h',
+        hoverinfo: otuLables
+      }];
+      
+      Plotly.newPlot('bar', data);
+});
