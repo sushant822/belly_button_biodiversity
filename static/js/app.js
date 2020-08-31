ca7e8156242f29ae59e8e2bd49694b928ecf10d5
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-08-30 18:09:00 -0600

    Horizintal bar graph working for first set

--- static/js/app.js
+++ static/js/app.js
@@ -5,16 +5,17 @@ function unpack(rows, index) {
   }
 
 d3.json(url).then(function(data) {
-    var sampleValues = data.samples[0].sample_values.slice(0, 10);
-    var otuIDS = data.samples[0].otu_ids;
-    var otuLables = data.samples[0].otu_labels;
-    //console.log(otuLables);
+    var sampleValues = data.samples[0].sample_values.slice(0, 10).reverse();
+    var otuIDS = data.samples[0].otu_ids.slice(0, 10).reverse();
+    var otuLables = data.samples[0].otu_labels.slice(0, 10);
+    var OTU_id = otuIDS.map(value => "OTU " + value)
+    //console.log(data);
     var data = [{
         type: 'bar',
         x: sampleValues,
-        y: otuLables,
+        y: OTU_id,
         orientation: 'h',
-        hoverinfo: otuLables
+        text: otuLables
       }];
       
       Plotly.newPlot('bar', data);
