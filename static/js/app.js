0a4e22f0485ecd2bdd77c9a889b8dd42508600e0
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-09-01 21:48:20 -0600

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,52 @@
 var url = "samples.json";
 
-d3.json(url).then(function(data) {
-    var sampleValues = data.samples[0].sample_values.slice(0, 10).reverse();
-    var otuIDS = data.samples[0].otu_ids.slice(0, 10).reverse();
-    var otuLables = data.samples[0].otu_labels.slice(0, 10);
-    var OTU_id = otuIDS.map(value => "OTU " + value)
-    console.log(OTU_id);
-    var data = [{
-        type: 'bar',
-        x: sampleValues,
-        y: OTU_id,
-        orientation: 'h',
-        text: otuLables
-      }];
+function plotData(id) {
+  d3.json(url).then(data => {
+    var samples = data.samples.filter(s => s.id.toString() === id)[0];
+    var sampleValues = samples.sample_values.slice(0, 10).reverse();
+    var otuIDS = samples.otu_ids.slice(0, 10).reverse();
+    var otuLables = samples.otu_labels.slice(0, 10);
+    var OTU_id = otuIDS.map(value => "OTU " + value);
       
-      var layout = {
-        title: "Top 10 OTU",
-        yaxis:{tickmode:"linear"},
+    var trace1 = {
+      type: 'bar',
+      x: sampleValues,
+      y: OTU_id,
+      orientation: 'h',
+      text: otuLables
+      };
+        
+    var layout1 = {
+      title: "Top 10 OTU",
+      yaxis:{tickmode:"linear"},
     };
 
-    Plotly.newPlot('bar', data, layout);
-});
+    var trace2 = {
+      x: OTU_id,
+      y: sampleValues,
+      mode: "markers",
+      marker: {
+        size: sampleValues,
+        color: OTU_id 
+      },
+      text: otuLables
+    };
+
+    var layout2 = {
+      xaxis: {
+        title: "OTU ID"
+      },
+      height: 600,
+      width: 1000
+    };
+
+    Plotly.newPlot("bar", trace1, layout1);
+    Plotly.newPlot("bubble", trace2, layout2);
+
+    console.log(OTU_id);
+    console.log(otuLables);
+    console.log(sampleValues);
+  });
+};
+
+plotData();
