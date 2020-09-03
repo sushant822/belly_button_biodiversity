fcf7fec5dce9d7c1dccf4a2a0cd9d485588a86d5
Sushant Deshpande <41443708+sushant822@users.noreply.github.com>
2020-09-02 20:05:46 -0600

    Updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 //Set the path to our data
-var url = "samples.json";
+var url = "./static/js/samples.json";
 
 //Create a afunction that will plot all the necessary graphs
 function plotData(id) {
@@ -29,9 +29,95 @@ function plotData(id) {
       yaxis:{tickmode:"linear"},
     };
 
-    //Create trace for Gauge Chart
+    //Gauge Chart
     var wfreq = data.metadata.map(data => data.wfreq)
-    var traceGauge = [{
+
+    // *** BONUS Gauge ***
+    var level = parseFloat(wfreq) * 20;
+    
+    //Mathematical functions needed to plot the pie/gauge
+    var degrees = 180 - level;
+    var radius = 0.5; 
+    var radians = (degrees * Math.PI) / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    var mainPath = "M-.0 -0.05 L  .0 0.05 L";
+    var pathX = String(x);
+    var space = " ";
+    var pathY = String(y);
+    var pathEnd = " Z";
+    var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+    //Create trace for Gauge chart
+    var traceGauge = [
+        {
+          //Let's create the dot for the indicator
+            x:[0],
+            y:[0],
+            marker: { size: 15, color: "#800000" },
+            showlegend: false,
+            text: level
+        },
+        /*We'll segement our pie chart into 10 pieces, the last piece would be the bottom half of the pie which we'll color white (#FFFFFF). Since we don't want to show any label at the bottom of the pie/gauge, we'll enter an empty string in text and label. Then we'll add 'hole' property to it so that it looks like a meter and we'll set showlegend to false.*/
+        {
+            values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+            rotation: 90,
+            text:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            textinfo: "text",
+            textposition: "inside",
+            marker: {
+                colors: [
+                    "#006400",
+                    "#556B2F",
+                    "#6B8E23",
+                    "#8FBC8F",
+                    "#98FB98",
+                    "#F0E68C",
+                    "#D2B48C",
+                    "#F5DEB3",
+                    "#FFE4C4",
+                    "#FFFFFF"
+                ]
+            },
+            labels:["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+            hoverinfo: "label",
+            hole: 0.45,
+            type: "pie",
+            showlegend: false
+        }
+    ]
+    //Create layout for Gauge Chart
+    var layoutGauge = {
+        shapes: [
+            {
+                type: "path",
+                path: path,
+                fillcolor: "#800000",
+                line: {
+                  color: "#800000"
+                }
+            }
+        ],
+        title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+        /*We'll set zero line, tick lables and gride to false on both axis. */
+        xaxis: {
+            zeroline:false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false,
+            showticklabels: false,
+            showgrid: false,
+            range: [-1, 1]
+        }
+    }
+
+    // *** Standard Gauge ***
+
+    //Create trace for Gauge chart
+    /*var traceGauge = [{
       domain: {
         x: [0, 1], y: [0, 1]
       },
@@ -62,7 +148,7 @@ function plotData(id) {
     var layoutGauge = {
       width: 700,
       height: 600
-    };
+    };*/
 
     //Create trace for Bubble Chart
     var traceBubble = {
@@ -103,6 +189,7 @@ function infoData(id) {
 
     Object.entries(result).forEach(data => {
       demoGraphic.append("h5").text(data[0] + ": " + data[1] + "\n");
+      console.log(data);
     });
   });
 };
@@ -116,7 +203,7 @@ function optionChanged(id) {
 function init() {
   var dropdown = d3.select("#selDataset");
   d3.json(url).then(data => {
-      data.names.forEach(function(name) {
+      data.names.forEach(name => {
           dropdown.append("option").text(name).property("value");
       });
       plotData(data.names[0]);
